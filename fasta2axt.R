1031b6afe5e7fa6bf996bcb59def98c74a84b9dc
ShaolinXU <56570371+ShaolinXU@users.noreply.github.com>
2020-11-05 13:53:25 +0800

    Update fasta2axt.R

--- fasta2axt.R
+++ fasta2axt.R
@@ -7,23 +7,21 @@ library(seqinr)
 
 args = commandArgs(trailingOnly=TRUE)
 
-seq_file <- read.fasta(args[1],as.string = T)
+output <- args[2] # 输出文件名称
 
-output <- args[2] # 输出文件名称 output file name 
+seq_file <- read.fasta(args[1],as.string = T)
 
 N_seq <- length(seq_file)
 
-ref_seq <- seq_file[[which(attr(seq_file,"name")==args[3])]] #args[3] 指定用作参照的物种 taxa header as reference seq
-
-ref_seq_seq <- ref_seq[1] # sequence of ref seq
+ref_seq <- seq_file[[which(attr(seq_file,"name")==args[3])]] #args[3] 指定用作参照的物种
+ref_seq_seq <- ref_seq[1]
 
 for (i in 1:N_seq){
   if(attr(seq_file[[i]],"name")!=args[3]){
     comp_pair <- paste(args[3],attr(seq_file[[i]],"name"),sep = "-")
-    write(comp_pair,file = args[2],append = T) # output file name in args[2]
+    write(comp_pair,file = args[2],append = T) # output file name in args[4]
     write(ref_seq_seq,file = args[2],append = T)
-    write(seq_file[[i]][1],file = args[2],append=T)
-    write("\n",file = args[2],append=T)
+    write(paste0(seq_file[[i]][1],"\n"),file = args[2],append=T)
+    #write("\n",file = args[2],append=T)
   }
 }
-
