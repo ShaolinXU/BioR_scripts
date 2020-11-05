c45b49feab500fc7452aca150abdbe5e7ac5105f
ShaolinXU <56570371+ShaolinXU@users.noreply.github.com>
2020-11-05 13:28:44 +0800

    Update fasta2axt.R
    convert fasta file to axt file used in kaks_calculator 2.0

--- fasta2axt.R
+++ fasta2axt.R
@@ -7,22 +7,23 @@ library(seqinr)
 
 args = commandArgs(trailingOnly=TRUE)
 
-output <- args[2] # 输出文件名称
-
 seq_file <- read.fasta(args[1],as.string = T)
-test <- seq_file[[1]]
+
+output <- args[2] # 输出文件名称 output file name 
 
 N_seq <- length(seq_file)
 
-ref_seq <- seq_file[[which(attr(seq_file,"name")==args[3])]] #args[3] 指定用作参照的物种
-ref_seq_seq <- ref_seq[1]
+ref_seq <- seq_file[[which(attr(seq_file,"name")==args[3])]] #args[3] 指定用作参照的物种 taxa header as reference seq
+
+ref_seq_seq <- ref_seq[1] # sequence of ref seq
 
 for (i in 1:N_seq){
   if(attr(seq_file[[i]],"name")!=args[3]){
     comp_pair <- paste(args[3],attr(seq_file[[i]],"name"),sep = "-")
-    write(comp_pair,file = args[2],append = T) # output file name in args[4]
+    write(comp_pair,file = args[2],append = T) # output file name in args[2]
     write(ref_seq_seq,file = args[2],append = T)
     write(seq_file[[i]][1],file = args[2],append=T)
     write("\n",file = args[2],append=T)
   }
 }
+
